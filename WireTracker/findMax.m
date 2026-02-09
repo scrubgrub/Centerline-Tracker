d6ca73d647da831fa39f76fda8a1e0a5bc824aa5
Jade L <jlarivi1@asu.edu>
2026-02-09 14:00:41 -0700

    Update findMax.m
    one major bug was fixed regarding indexing the proper pixel in the image gradient, and the new upscaling function scaleImg was incorporated here. received general polish updates.

--- WireTracker/findMax.m
+++ WireTracker/findMax.m
@@ -1,20 +1,26 @@
 %% helper function to detect closest local maximum of 2D image
-% Jade Lariviere | last modified Mar. 24, 2025
+% Jade Lariviere | last modified Jan. 21, 2026
 
 function [maxVal,maxUV,cMap] = findMax(Image,startPt,maxFlags,maxIt,SearchType)
-% this function calculates the gradient of an input 2D image and iterates
+% this function calculates the gradient of the input Image and iterates
 % based on flagSensitivity or until MaxIt to find the closest local max. it
 % returns the maximum value found and the associated coordinates (u,v). the
 % SearchType determines what method to use to determine the maximum value
-% in an image ROI, which is either absolute or local search.
+% in an image ROI, which can be customized. ===============================
 
-img_dims = size(Image); % grab image dimensions info
+scaleFactor = 5; % how much bigger to make upscaled img; 1 = same
+interpMethod = 'bicubic'; % interp method for imresize
+
+% make higher pixel image via interp, scale startPt
+[newImage,newImg_dim] = scaleImg(Image,scaleFactor,interpMethod);
+newStartPt = startPt.*scaleFactor;
+img_dim = size(Image); % store original image dims
 
 % calculate image gradient from best ROI ----------------------------------
-[fv,fu] = gradient(Image); % swap fv/fu due to gradient() definition
+[fv,fu] = gradient(newImage); % swap fv/fu due to gradient() definition
 
 % RGB color map for viewing image derivatives
-cMap(1:img_dims(1),1:img_dims(2),1:3) = 0;
+cMap(1:newImg_dim(1),1:newImg_dim(2),1:3) = 0;
     c_fu = fu; c_fv = fv;
     c_fu(fu<0) = fu(fu<0)./abs(min(fu.*2,[],'all'));
     c_fu(fu>0) = fu(fu>0)./max(fu.*2,[],'all');
@@ -26,57 +32,81 @@ cMap(1:img_dims(1),1:img_dims(2),1:3) = 0;
 switch SearchType % determine which max determination method to use
 case 'Absolute'
 % grab max value ----------------------------------------------------------
-[maxVal,idx_max] = max(Image,[],'all');
-[max_u, max_v] = ind2sub(img_dims,idx_max);
-    maxUV = [max_u, max_v];
+[maxVal,idx_max] = max(newImage,[],'all');
+[max_u, max_v] = ind2sub(newImg_dim,idx_max);
+    maxUV = [max_u, max_v]./scaleFactor; % scale back to Image dims!
 case 'Local'
 % search loop -------------------------------------------------------------
-c_new = startPt; % starting coordinates (P = origin)
-try value_new = Image(round(c_new(1)),round(c_new(2))); % pixel value
+c_new = newStartPt; % starting coordinates (P = origin)
+try value_new = newImage(ceil(c_new(1)),ceil(c_new(2))); % pixel value
 catch % clamp coordinates to image dimensions
-    warning('P(u,v) clamped to image dimensions. Consider increasing ImageDims.');
-    c_new = [min(c_new(1),img_dims(1)), min(c_new(2),img_dims(2))]; % clamp
-    value_new = Image(c_new(1),c_new(2));
+    warning('P(u,v) indexed past image dimensions. Consider increasing ImageDims.');
+    c_new = [min(c_new(1),newImg_dim(1)), min(c_new(2),newImg_dim(2))]; % clamp
+    value_new = newImage(c_new(1),c_new(2));
 end
 
 flagCount = 0; flagThresh = maxFlags; i = 0;
 while (flagCount < flagThresh) && (i < maxIt)
     % update pixel (centroid) & intensity value from previous loop
-    c = c_new;
-    value = value_new;
-    
-    ascent = [fu(round(c(1)),round(c(2))), ...
-        fv(round(c(1)),round(c(2)))]; % store closest direction of ascent
-    ascent_dir = ascent/vecnorm(ascent,2,2);
-        if isnan(ascent_dir); ascent_dir = [0 0]; end % prevent NaNs
-    % step to new pixel/centroid (logs cale clamped), get intensity value
+    c = c_new; value = value_new;
+    % store direction of ascent
+    ascent = [fu(min(ceil(c(1)),newImg_dim(1)),min(ceil(c(2)),newImg_dim(2))), ...
+        fv(min(ceil(c(1)),newImg_dim(1)),min(ceil(c(2)),newImg_dim(2)))];
+    ascent_dir = ascent/vecnorm(ascent,2,2); % normalize
+
+    if isnan(ascent_dir) % what if ascent of zero at c?
+        % check neighboring pixels for valid ascent, average
+        neighbors_ascent = zeros(3,3,2); % 3x3 grid for 1) fu & 2) fv
+        neighbors_ascent(:,:,1) = fu(min(ceil(c(1)-1:c(1)+1),newImg_dim(1)), ...
+            min(ceil(c(2)-1:c(2)+1),newImg_dim(2)));
+        neighbors_ascent(:,:,2) = fv(min(ceil(c(1)-1:c(1)+1),newImg_dim(2)), ...
+            min(ceil(c(2)-1:c(2)+1),newImg_dim(2)));
+        neighbors_ascent(isnan(neighbors_ascent)) = 0; % NaN failsafe
+
+        ascent = squeeze(mean(neighbors_ascent,[1 2]))'; % average ascent
+            ascent_dir = ascent_dir/vecnorm(ascent,2,2); % normalize
+        if isnan(ascent_dir), ascent_dir = [0 0]; end % final failsafe
+    end
+    % step to new pixel (log scaling)
     c_new = c + ascent_dir.*(abs(log(vecnorm(ascent,2,2)+5))./4);
-    value_new = Image(round(c_new(1)),round(c_new(2)));
+        % clamp c to within image bounds to prevent index errors!
+        c_new = [max(min(c_new(1),newImg_dim(1)),1) max(min(c_new(2),newImg_dim(2)),1)];
+    % what is the value at the pixel where c is?
+    value_new = newImage(min(ceil(c_new(1)),newImg_dim(1)), ...
+        min(ceil(c_new(2)),newImg_dim(2)));
     % compare new_value to value; always seek higher value
     if value_new <= value
-        flagCount = flagCount + 1; % overshot max, internally track it
+        flagCount = flagCount + 1; % settled or overshot, track it
     end
     i = i + 1; % increment counter; repeat while loop conditions are true
 end
-% return final max value + coordinates ====================================
-maxVal = value_new; maxUV = c_new;
+% return final max coordinates + value ====================================
+% scale back to original Image dimensions + use intensity in original
+maxUV = c_new./scaleFactor;
+maxVal = Image(min(ceil(maxUV(1)),img_dim(1)), ...
+    min(ceil(maxUV(2)),img_dim(2))); % 
 otherwise
 error('Input argument %s for SearchType not recognized.',SearchType);
 end
+
 % plotting for debugging ==================================================
 %{
-max_val = 2*max(Image,[],'all');
+img = Image;
+max_val = 2*max(img,[],'all');
 valRange = [0 max_val*1.1]; % intensity scaling for plotting
-Image(round(maxUV(1)),round(maxUV(2))) = max_val; % highlight found pixel
+derivRange = [min([fu fv],[],"all")*1.1, max([fu fv],[],"all")*1.1];
+% highlight found pixel
+img(min(ceil(maxUV(1)),img_dim(1)), ...
+    min(ceil(maxUV(2)),img_dim(2))) = max_val;
     
-figure(13); % transpose images for plotting (MATLAB hates being normal)
-    subplot(2,2,1); imshow(Image',valRange); set(gca,"YDir","normal");
-        title('Image');
+figure(14); % transpose images for plotting (MATLAB hates being normal)
+    subplot(2,2,1); imshow(img',valRange); set(gca,"YDir","normal");
+        title('Original Image (POI Highlighted)');
     subplot(2,2,2); imshow(permute(cMap,[2 1 3])); set(gca,"YDir","normal");
-        title('Colormap of Image Derivative');
-    subplot(2,2,3); imshow(fu',[-0.5 0.5]); set(gca,"YDir","normal");
+        title('Derivative Colormap of Scaled Image');
+    subplot(2,2,3); imshow(fu',derivRange); set(gca,"YDir","normal");
         xlabel('u'); ylabel('v'); title('F_u');
-    subplot(2,2,4); imshow(fv',[-0.5 0.5]); set(gca,"YDir","normal");
+    subplot(2,2,4); imshow(fv',derivRange); set(gca,"YDir","normal");
         xlabel('u'); ylabel('v'); title('F_v');
 %}
 end
