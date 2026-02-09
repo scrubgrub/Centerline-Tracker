09f2f958d09a7296eed7711700564cdbb1105527
Jade L <jlarivi1@asu.edu>
2026-02-09 13:47:42 -0700

    Update dialogTrackingCheck.m
    added more features from the original, including dynamic listing details of tracked centerline for branch detection. there is just general additional polish to the code and UI.

--- WireTracker/dialogTrackingCheck.m
+++ WireTracker/dialogTrackingCheck.m
@@ -1,23 +1,45 @@
 %% helper function to create a dialog box for centroid finalization
-% Jade Lariviere | last modified Mar. 8, 2025
+% Jade Lariviere | last modified Dec. 7, 2025
 
-function answerYN = dialogTrackingCheck(InputFigure)
-% formatting figure axes + etc.
+function [answer] = dialogTrackCheck(InputFigure,figSet,TrackStats)
+% setting up dynamic labels -----------------------------------------------
+num_line = numel(findobj(gca,'Type','Text'));
+[labels{1:num_line}] = deal('');
+    labels = [{''},{'First Track'},{'Start'},labels(:)',{'End'}];
+
+track_stats = [(1:numel(TrackStats))-1;TrackStats]';
+s_track = compose("Track: %d points",track_stats(1,2));
+
+% formatting figure axes + etc. -------------------------------------------
 p = InputFigure.CurrentAxes;
 set(p,'Position',[0.1,0.1,0.5,0.8]);
-xlabel('x'); ylabel('y'); zlabel('z'); view(3); grid minor;
-legend('','Wire Centroids','Start','End','Location','best');
+xlabel('x'); ylabel('y'); zlabel('z'); grid minor; axis equal; axis tight;
+if isempty(figSet), view(3); 
+    else, view(figSet{1}), set(gcf,"WindowState",figSet{2});
+end
+legend(labels,'Location','best');
 enableDefaultInteractivity(p);
 
 % tweak figure with editable boxes; show pop-up & ask for inputs ----------
 figure(InputFigure);
 fieldYN = uicontrol('Style','checkbox','Units','Normalized',...
      'Position',[.65 .45 .3 .05],'Tag','myedit',...
-     'String','Flip centroid order');
+     'String','Flip main centroid sequence');
     annotation('textbox',[.65 .51 .3 .05],...
     'String','Check your final centroids, and proceed if OK.',...
     'LineStyle','none','Margin',0,'FontSize',10,...
     'VerticalAlignment','bottom');
+    
+if length(TrackStats) > 1 % stats includes branch track point counts
+    s_branch = compose("Branch %d: %d points",track_stats(2:end,:));
+    annotation('textbox',[.65 .65 .3 .3],'String', [s_track;s_branch],...
+        'LineStyle','none','Margin',0,'FontSize',10,...
+        'VerticalAlignment','bottom','FontWeight','bold');
+else % just one track to display number of points
+    annotation('textbox',[.65 .7 .3 .3],'String', s_track,...
+        'LineStyle','none','Margin',0,'FontSize',10,...
+        'VerticalAlignment','bottom','FontWeight','bold');
+end
 
 OKbutton = uicontrol('Style','pushbutton','Units','Normalized',...
     'String','Continue to Export',...
@@ -27,8 +49,8 @@ OKbutton = uicontrol('Style','pushbutton','Units','Normalized',...
 uiwait(InputFigure); % prompt user input ----------------------------------
 
     function OK_callback(~,~,fieldData) % extract field input
-        answerYN = logical(fieldData.Value); % return logical
-        if answerYN % if user asked to flip centroids...
+        answer = logical(fieldData.Value); % return logical
+        if answer % if user asked to flip centroids...
             fprintf('Centroid order flipped! Continuing to save...\n');
         else
             fprintf('Continuing to save...\n');
